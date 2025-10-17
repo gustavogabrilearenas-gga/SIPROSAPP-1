260120bbce586509e47e9c75b830aa96677e1eb4
gustavogabrilearenas-gga <gustavgabriearenas@gmail.com>
2025-10-17 17:26:33 -0300

    yyyyyyyyyyyyyyyyyyyyyyy

--- backend/eventos/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.eventos'
-    verbose_name = 'Registro de Observaciones'
