674b8887a4c36cfb0bbcc340ebd6e8d5dfa58855
gustavogabrilearenas-gga <gustavgabriearenas@gmail.com>
2025-10-20 14:03:51 -0300

    Set creador automatico en admin de observaciones

--- backend/observaciones/admin.py
+++ backend/observaciones/admin.py
@@ -9,3 +9,10 @@ class ObservacionGeneralAdmin(admin.ModelAdmin):
     readonly_fields = ("fecha_hora", "creado_por")
     search_fields = ("texto", "creado_por__username")
     list_filter = ("fecha_hora",)
+
+    def save_model(self, request, obj, form, change):
+        """Asignar el usuario autenticado al crear y mantenerlo luego."""
+
+        if not change and not obj.creado_por_id:
+            obj.creado_por = request.user
+        super().save_model(request, obj, form, change)
